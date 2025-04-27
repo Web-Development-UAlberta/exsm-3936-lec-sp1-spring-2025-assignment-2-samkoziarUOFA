79812fc751f0321adf9a8818a04a83364d5b8727
Samantha Koziar <koziar@ualberta.ca>
2025-04-27 14:16:48 -0600

    Fix constructor, add rectangle class

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,37 +3,53 @@
 // eslint-disable-next-line no-unused-vars
 async function main() {
 
-// 
-  class Shape {
-    #colour = "white";
-
-    constructor(colour) {
-        if (this.constructor === Shape) {
-            throw new Error("Cannot instantiate abstract class Shape!.");
-        }
-        this.#colour = colour || this.#colour; // default to white
+ // Abstract Shape class
+ class Shape {
+  constructor(colour) {
+    if (new.target === Shape) {
+      throw new Error("Cannot instantiate abstract class Shape!");
     }
-
-    get colour() {
-        return this.#colour;
-    }
-
-    set colour(value) {
-        this.#colour = value;
-    }
+    this.colour = colour;
+  }
 
     // properties and errors
     get perimeter() {
         throw new Error(`Abstract method perimeter() must be implemented for ${this.constructor.name}`);
     }
-
     get area() {
         throw new Error(`Abstract method area() must be implemented for ${this.constructor.name}`);
     }
-
     contain() {
         throw new Error(`Abstract method contain() must be implemented for ${this.constructor.name}`);
     }
   }
+
+   // Rectangle class inheriting from Shape
+   class Rectangle extends Shape {
+    constructor(length, width, colour) {
+      super(colour);
+      this.length = length;
+      this.width = width;
+    }
+
+    get area() {
+      return this.length * this.width;
+    }
+
+    get perimeter() {
+      return 2*(this.length + this.width);
+    }
+
+    get isSquare() {
+      return this.length === this.width;
+    }
+
+    contain() {
+      const side = Math.max(this.length, this.width);
+      return new Rectangle(side, side, this.colour);
+    }
+  }
+
+
 
 }
