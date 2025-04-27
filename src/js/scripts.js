87c92cdd9a8a8d5f7281b87009d3acf2810ebc1b
Samantha Koziar <koziar@ualberta.ca>
2025-04-27 13:30:56 -0600

    Add Shape class and constructor for colour

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,6 +3,37 @@
 // eslint-disable-next-line no-unused-vars
 async function main() {
 
+// 
+  class Shape {
+    #colour = "white";
 
+    constructor(colour) {
+        if (this.constructor === Shape) {
+            throw new Error("Cannot instantiate abstract class Shape!.");
+        }
+        this.#colour = colour || this.#colour; // default to white
+    }
+
+    get colour() {
+        return this.#colour;
+    }
+
+    set colour(value) {
+        this.#colour = value;
+    }
+
+    // properties and errors
+    get perimeter() {
+        throw new Error(`Abstract method perimeter() must be implemented for ${this.constructor.name}`);
+    }
+
+    get area() {
+        throw new Error(`Abstract method area() must be implemented for ${this.constructor.name}`);
+    }
+
+    contain() {
+        throw new Error(`Abstract method contain() must be implemented for ${this.constructor.name}`);
+    }
+  }
 
 }
